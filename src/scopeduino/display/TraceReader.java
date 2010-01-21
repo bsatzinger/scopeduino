211069871bb19e2e708af4f7d71f30b4c2b9a611
Brian Satzinger <bsatzinger@gmail.com>
2010-01-21 14:21:13 -0600

    Added trace/second measurement

--- src/scopeduino/display/TraceReader.java
+++ src/scopeduino/display/TraceReader.java
@@ -23,11 +23,32 @@ public class TraceReader extends Thread {
 
     public void run()
     {
+        //Used to calculate the trace rate
+        int ntraces = 0;
+        long time = System.currentTimeMillis();
+
         while (arduino.connected)
         {
             double[] data = arduino.readTrace();
 
             Trace t = new Trace(data,0.2f, 0.2f, 1.0f, 0.5f,1);
+
+            //Calculate the current trace rate
+            ntraces++;
+            if (ntraces > 30)
+            {
+                double tracerate;
+                double dT;
+
+                dT = (System.currentTimeMillis() - time) / 1000.0;
+
+                tracerate = 30 / dT;
+
+                System.out.println("Trace Rate: " + tracerate + "traces/second. " + dT);
+
+                ntraces = 0;
+                time = System.currentTimeMillis();
+            }
 
             synchronized(traces)
             {
